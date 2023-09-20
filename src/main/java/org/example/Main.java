b97ef3fc83c4a44a6ada223c22a8f5435331860b
Smertex <kiratsvetkova102@gmail.com>
2023-09-20 15:51:16 +0300

    +mw

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        mainWindow.setSize(800,600);
+        mainWindow.setVisible(true);
     }
 }
