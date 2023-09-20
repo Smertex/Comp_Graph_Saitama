99254bcb2a7bf8b1400a8764c68ad0a1d57f93de
Smertex <kiratsvetkova102@gmail.com>
2023-09-20 16:04:26 +0300

    Star version

---
+++ src/main/java/org/example/DrawPanel.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import javax.swing.*;
+public class DrawPanel extends JPanel {
+
+    public DrawPanel(){
+
+    }
+
+}
--- src/main/java/org/example/MainWindow.java
+++ src/main/java/org/example/MainWindow.java
@@ -1,7 +1,16 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainWindow extends JFrame {
 
+    private final DrawPanel drawPanel;
+
+    public MainWindow() throws HeadlessException {
+
+        drawPanel = new DrawPanel();
+        this.add(drawPanel);
+
+    }
 }
