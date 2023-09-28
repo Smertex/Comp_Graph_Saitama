be46f054915b657530157051641e5f0cb4fcc5b7
Smertex <kiratsvetkova102@gmail.com>
2023-09-28 18:43:11 +0300

    Нарисовал складки на одежде

--- src/main/java/org/example/Elements/Saitama.java
+++ src/main/java/org/example/Elements/Saitama.java
@@ -19,6 +19,7 @@ public class Saitama{
         Color leaterColor = new Color(254,244,255);
         Color eyeColor = new Color(254, 247, 254);
         Color tightsColor = new Color(229, 172, 189);
+        Color button = new Color(134, 138, 209);
 
         //голова
         g.setColor(leaterColor);
@@ -100,7 +101,6 @@ public class Saitama{
         //Шея
         smallElements.moveTo((int)(x + 0.71 * w), (int)(y + 0.96 * h));
         smallElements.lineTo((int)(x + 0.71 * w), (int)(y + 1.1 * h));
-
         smallElements.moveTo((int)(x + 0.31 * w), (int)(y + 0.96 * h));
         smallElements.lineTo((int)(x + 0.31 * w), (int)(y + 1.1 * h));
         smallElements.curveTo((int)(x + 0.51 * w), (int)(y + 1.27 * h), (int)(x + 0.7 * w), (int)(y + 1.2 * h), (int)(x + 0.75 * w), (int)(y + 1.1 * h));
@@ -155,7 +155,6 @@ public class Saitama{
        colorBody.lineTo((int)(x + -0.09 * w), (int)(y + 1.48 * h));
 
        g.fill(colorBody);
-
        g.setColor(firstColor);
 
         //Обводка правой части плаща
@@ -168,7 +167,6 @@ public class Saitama{
         сloakStroke.curveTo((int)(x + 0.04 * w), (int)(y + 1.13 * h), (int)(x + 0.04 * w), (int)(y + 1.13 * h), (int)(x + 0.01 * w), (int)(y + 1.15 * h));
         сloakStroke.curveTo((int)(x + -0.05 * w), (int)(y + 1.15 * h), (int)(x + -0.05 * w), (int)(y + 1.15 * h), (int)(x + -0.08 * w), (int)(y + 1.18 * h));
         сloakStroke.lineTo((int)(x + -0.3 * w), (int)(y + 1.47 * h));
-
 
         сloakStroke.moveTo((int)(x + 0.31 * w), (int)(y + 1.1 * h));
         сloakStroke.curveTo((int)(x + 0.41 * w), (int)(y + 1.15 * h), (int)(x + 0.45 * w), (int)(y + 1.24 * h), (int)(x + 0.39 * w), (int)(y + 1.27 * h));
@@ -185,20 +183,63 @@ public class Saitama{
         сloakStroke.curveTo((int)(x + 0.8 * w), (int)(y + 1.19 * h), (int)(x + 0.76 * w), (int)(y + 1.16 * h), (int)(x + 0.71 * w), (int)(y + 1.08 * h));
         g.draw(сloakStroke);
 
-        //Правая рука заливка
-        GeneralPath сolorHands = new GeneralPath();
-        сolorHands.moveTo((int)(x + -0.08 * w), (int)(y + 1.18 * h));
-        сolorHands.curveTo((int)(x + -0.08 * w), (int)(y + 1.27 * h), (int)(x + -0.12 * w), (int)(y + 1.33 * h), (int)(x + -0.09 * w), (int)(y + 1.47 * h));
-        сolorHands.lineTo((int)(x + 0.1 * w), (int)(y + 1.48 * h));
-        сolorHands.curveTo((int)(x + 0.1 * w), (int)(y + 1.48 * h), (int)(x + 0.14 * w), (int)(y + 1.36 * h), (int)(x + 0.11 * w), (int)(y + 1.25 * h));
+        //Правая рука обводка
+        GeneralPath hands = new GeneralPath();
+        hands.moveTo((int)(x + -0.08 * w), (int)(y + 1.18 * h));
+        hands.curveTo((int)(x + -0.08 * w), (int)(y + 1.27 * h), (int)(x + -0.12 * w), (int)(y + 1.33 * h), (int)(x + -0.09 * w), (int)(y + 1.47 * h));
+        hands.lineTo((int)(x + 0.1 * w), (int)(y + 1.48 * h));
+        hands.curveTo((int)(x + 0.1 * w), (int)(y + 1.48 * h), (int)(x + 0.14 * w), (int)(y + 1.36 * h), (int)(x + 0.11 * w), (int)(y + 1.25 * h));
 
-        //Левая рука заливка
+        //Левая рука обводка
+        hands.moveTo((int)(x + 1.047 * w), (int)(y + 1.15 * h));
+        hands.curveTo((int)(x + 1.08 * w), (int)(y + 1.25 * h), (int)(x + 1.07 * w), (int)(y + 1.35 * h), (int)(x + 1.15 * w), (int)(y + 1.48 * h));
+        hands.lineTo((int)(x + 0.9 * w), (int)(y + 1.48 * h));
+        hands.curveTo((int)(x + 0.9 * w), (int)(y + 1.45 * h), (int)(x + 0.9 * w), (int)(y + 1.35 * h), (int)(x + 0.94 * w), (int)(y + 1.28 * h));
+        g.draw(hands);
 
-        сolorHands.moveTo((int)(x + 1.047 * w), (int)(y + 1.15 * h));
-        сolorHands.curveTo((int)(x + 1.08 * w), (int)(y + 1.25 * h), (int)(x + 1.07 * w), (int)(y + 1.35 * h), (int)(x + 1.15 * w), (int)(y + 1.48 * h));
-        сolorHands.lineTo((int)(x + 0.9 * w), (int)(y + 1.48 * h));
-        сolorHands.curveTo((int)(x + 0.9 * w), (int)(y + 1.45 * h), (int)(x + 0.9 * w), (int)(y + 1.35 * h), (int)(x + 0.94 * w), (int)(y + 1.28 * h));
-        g.draw(сolorHands);
+        //Пуговицы на плаще
+
+        g.setColor(button);
+
+        g.fillOval((int)(x + 0.82 * w),(int)(y + 1.1 * h), (int)(w / 5 - 17),(int)(h / 6 - 25));
+        g.fillOval((int)(x + 0.22 * w),(int)(y + 1.17 * h), (int)(w / 5 - 17),(int)(h / 6 - 25));
+
+        g.setColor(firstColor);
+
+        g.drawOval((int)(x + 0.82 * w),(int)(y + 1.1 * h), (int)(w / 5 - 17),(int)(h / 6 - 25));
+        g.drawOval((int)(x + 0.22 * w),(int)(y + 1.17 * h), (int)(w / 5 - 17),(int)(h / 6 - 25));
+
+        //Складки на одежде
+
+        GeneralPath folds = new GeneralPath();
+        folds.moveTo((int)(x + 0.24 * w), (int)(y + 1.09 * h));
+        folds.curveTo((int)(x + 0.32 * w), (int)(y + 1.18 * h), (int)(x + 0.32 * w), (int)(y + 1.18 * h), (int)(x + 0.32 * w), (int)(y + 1.18 * h));
+
+        folds.moveTo((int)(x + 0.26 * w),(int)(y + 1.17 * h));
+        folds.lineTo((int)(x + 0.24 * w),(int)(y + 1.15 * h));
+
+        folds.moveTo((int)(x + 0.18 * w),(int)(y + 1.14 * h));
+        folds.lineTo((int)(x + 0.24 * w),(int)(y + 1.18 * h));
+
+        folds.moveTo((int)(x + 0.22 * w),(int)(y + 1.21 * h));
+        folds.lineTo((int)(x + 0.01 * w),(int)(y + 1.15 * h));
+
+        folds.moveTo((int)(x + 0.81 * w),(int)(y + 1.04 * h));
+        folds.curveTo((int)(x + 0.86 * w),(int)(y + 1.06 * h), (int)(x + 0.86 * w),(int)(y + 1.08 * h), (int)(x + 0.85 * w),(int)(y + 1.1 * h));
+
+        folds.moveTo((int)(x + 0.9 * w), (int)(y + 1.05 * h));
+        folds.curveTo((int)(x + 0.9 * w), (int)(y + 1.07 * h), (int)(x + 0.91 * w), (int)(y + 1.07 * h), (int)(x + 0.9 * w), (int)(y + 1.1 * h));
+
+        folds.moveTo((int)(x + 0.95 * w), (int)(y + 1.07 * h));
+        folds.lineTo((int)(x + 0.96 * w), (int)(y + 1.11 * h));
+
+        folds.moveTo((int)(x + 0.94 * w), (int)(y + 1.16 * h));
+        folds.lineTo((x + w), (int)(y + 1.12 * h));
+
+        folds.moveTo((int)(x + 0.91 * w), (int)(y + 1.38 * h));
+        folds.curveTo((int)(x + 0.95 * w), (int)(y + 1.33 * h), (int)(x + 0.94 * w), (int)(y + 1.31 * h), (int)(x + 0.99 * w), (int)(y + 1.29 * h));
+
+        g.draw(folds);
 
 
     }
